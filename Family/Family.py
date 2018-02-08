534bbdc6942a6b2dab6168b082a71f8e1d9cc4ef
Moran Sisso <moran.sisso@gmail.com>
2018-02-08 14:44:55 +0200

    Added info to readme file

---
+++ Family/Family.py
@@ -0,0 +1,18 @@
+print ("Hello Sisso Family :)")
+
+age = int(input("Please type your Age:"))
+
+if age == 10 :
+       print("Hello Ilay Sisso")
+
+else:
+  if age == 8:
+   print("Hello Yeheli Sisso")
+
+  else:
+      if age == 2:
+       print("Hello Ariel Sisso, you are too little to use the Computer !!!")
+
+      else:
+
+        print ("I Don't Know You")
